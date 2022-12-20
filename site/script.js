4a6f7ea97a9dda0751fd3d0722e0ffe3719c4e72
William Boily <william.boily@hotmail.ca>
2022-12-19 21:40:53 -0500

    add base plus try one of showing question 1

--- site/script.js
+++ site/script.js
@@ -1,1 +1,34 @@
-console.log("Choo choo!");
+
+function clearScreen(screenId){
+	document.getElementById(screenId).innerHTML = '';
+}
+
+function hideScreen(screenId){
+	document.getElementById(screenId).hidden = true;
+}
+
+function showScreen(screenId){
+	document.getElementById(screenId).hidden = false;
+}
+
+document.getElementById("btn-start").onclick = function() {
+	hideScreen("screen-1");
+	showScreen("screen-2");
+};
+
+document.querySelectorAll("#screen-2 > button").forEach((element) => {
+	element.onclick = function(){
+		screen2Answer(this.value)
+	}
+});
+
+function screen2Answer(choice){
+	switch (choice) {
+		case "pigeon":
+			
+			break;
+	
+		default:
+			break;
+	}
+}
